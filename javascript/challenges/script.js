f60887ff4cae84567036acfc11f4033fcb0c4734
peacemakafui <35346710+peacemakafui@users.noreply.github.com>
2019-12-03 09:29:21 +0000

    Add files via upload

---
+++ javascript/challenges/script.js
@@ -0,0 +1,161 @@
+//challenge one: your age in days
+
+function ageInDays(){
+	var birthYear = prompt('what year were you born...Good friend');
+	var ageInDayss = (2019 - birthYear) * 365;
+	var h1 = document.createElement('h1');
+	var textAnswer = document.createTextNode('You are ' + ageInDayss + ' days old.');
+    h1.setAttribute('id', 'ageInDays');
+	h1.appendChild(textAnswer);
+	document.getElementById('flex-box-result').appendChild(h1);
+}
+
+function reset(){
+	document.getElementById('ageInDays').remove();
+}
+
+
+
+//challenge two: image generator
+function generateImage(){
+	var image = document.createElement('img');
+	var div = document.getElementById('flex-image-gen');
+	image.src="list.PNG";
+	div.appendChild(image);
+}
+
+function clearImage(){
+	document.getElementById('flex-image-gen').remove();
+	
+}
+
+
+
+//challenge Three: Rock, Paper, Scissprs
+function rpsGame(yourChoice){
+	console.log(yourChoice);
+	var humanChoice, botChoice;
+	humanChoice = yourChoice.id;
+	
+	botChoice = numberToChoice(randToRpsInt());
+	console.log('Computer choice: ', botChoice);
+	
+	results = decideWinner(humanChoice, botChoice);//[0,1] human lost | bot won
+	console.log(results);
+
+	message = finalMessage(results);
+	console.log(message);
+	rpsFrontEnd(yourChoice.id, botChoice, message);
+
+}
+
+function randToRpsInt(){
+	return Math.floor(Math.random() * 3);
+}
+
+function numberToChoice(number){
+	return ['rock','paper','scissors'][number];
+}
+
+function decideWinner(yourChoice, computerChoice){
+	var rpsDatabase={
+		'rock':{'scissors':1,'rock':0.5,'paper':0},
+		'paper':{'rock':1,'paper':0.5, 'scissors':0},
+		'scissors':{'paper':1, 'scissors':0.5, 'rock':0}
+	};
+
+	var yourScore = rpsDatabase[yourChoice][computerChoice];
+	var computerScore = rpsDatabase[computerChoice][yourChoice];
+
+	return[yourScore, computerScore];
+}
+
+function finalMessage([yourScore, computerScore]){
+	if(yourScore === 0){
+		return{'message': 'You lost!', 'color': 'red'};
+	}else if(yourScore === 0.5){
+		return{'message':'you tied!', 'color': 'yellow' };
+	}else {
+		return{'message': 'You won!', 'color': 'green'};
+	}
+} 
+
+function rpsFrontEnd(humanImageChoice, botImageChoice, finalMessage){
+	var imagesDatabase={
+		'rock':document.getElementById('rock').src,
+		'paper': document.getElementById('paper').src,
+		'scissors': document.getElementById('scissors').src
+	}
+
+	document.getElementById('rock').remove();
+	document.getElementById('paper').remove();
+	document.getElementById('scissors').remove();
+
+	var humanDiv = document.createElement('div');
+	var botDiv = document.createElement('div');
+	var messageDiv = document.createElement('div');
+
+	humanDiv.innerHTML = "<img src='" + imagesDatabase[humanImageChoice] + "' height=150 width=150 style='box-shadow: 0px 10px 50px rgba(37, 50, 233, 1);'>"
+	messageDiv.innerHTML = "<h1 style='color: " + finalMessage['color'] + "; font-sez: 60px; padding: 30px; '>" + finalMessage['message'] + "</h1>"
+	botDiv.innerHTML = "<img src='" + imagesDatabase[botImageChoice] + "' height=150 width=150 style='box-shadow: 0px 10px 50px rgba(243, 38, 24, 1);'>"
+	
+	document.getElementById('flex-box-rps-div').appendChild(humanDiv);
+	document.getElementById('flex-box-rps-div').appendChild(messageDiv);
+	document.getElementById('flex-box-rps-div').appendChild(botDiv);
+
+
+  
+}
+
+
+//Challenge four: Change the Color of All buttons
+var all_buttons = document.getElementsByTagName('button');
+console.log(all_buttons);
+
+var copyAllButtons = [];
+for(let i=0; i < all_buttons.length; i++){
+	copyAllButtons.push(all_buttons[i].classList[1]);
+}
+
+function buttonColorChange(buttonThingy){
+	if(buttonThingy.value === 'red'){
+		buttonRed();
+	}else if(buttonThingy.value === 'green'){
+		buttonGreen();
+	}else if(buttonThingy.value ==='reset'){
+		buttonColorReset();
+	}else if(buttonThingy.value === 'random'){
+		randomColors();
+	}
+}
+
+function buttonRed(){
+	for (let i=0; i < all_buttons.length; i++){
+		all_buttons[i].classList.remove(all_buttons[i].classList[1]);
+		all_buttons[i].classList.add('btn-danger');
+	}
+}
+
+function buttonGreen(){
+	for (let i=0; i < all_buttons.length; i++){
+		all_buttons[i].classList.remove(all_buttons[i].classList[1]);
+		all_buttons[i].classList.add('btn-success');
+	}
+}
+
+function buttonColorReset(){
+	for (let i =0; i < all_buttons.length; i++){
+		all_buttons[i].classList.remove(all_buttons[i].classList[1]);
+		all_buttons[i].classList.add(copyAllButtons[i]);
+	}
+}
+
+function randomColors(){
+	let choices = ['btn-primary', 'btn-danger', 'btn-success','btn-warning']
+
+	for(let i=0; i < all_buttons.length; i++){
+		let randomNumber = Math.floor(Math.random() * 4);
+		all_buttons[i].classList.remove(all_buttons[i].classList[1]);
+		all_buttons[i].classList.add(choices[randomNumber]);
+	}
+}
---
+++ javascript/home.js
@@ -0,0 +1,66 @@
+/*
+var age = prompt('what is your age');
+var name = prompt('what is your name');
+document.getElementById('someText').innerHTML = age;
+document.getElementById('text').innerHTML = name;
+*/
+
+//........functions.........
+//function without arguments
+function myFunction(yourName){
+	 
+	console.log('hello ' + yourName);
+}
+//var name = prompt('what is your name');
+//myFunction(name);
+
+//function with arguments
+
+function sumNumbers(num1,num2){
+	console.log(num1 + num2);
+}
+//sumNumbers(10,10);
+
+
+//...........loops..........
+
+/*//while loops\
+var num = 0;
+while(num < 100){
+	num+=1;
+	console.log(num)
+}
+*/
+
+/*// for loops
+
+for(let num = 0; num <=100; num++){
+	console.log(num)
+}
+*/
+
+/*//arrays
+let emptyArray = new Array();
+for(let i =0; i <=10; i++){
+	emptyArray.push(i);
+}
+console.log(emptyArray);
+*/
+ /*//objects..
+ let student = {
+ 	first:'maka',
+ 	last: 'peace',
+ 	age:21,
+ 	heigt:175,
+
+ 	//getters..
+ 	studentInfo: function(){
+ 		return this.first + '\n' + this.last;
+ 	}
+
+ };
+ console.log(student.first);
+ console.log(student.studentInfo());
+*/
+
+
